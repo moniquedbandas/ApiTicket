0421c08cd2a30adec006cdd2cbd228217a7256ec
Monique Dias <moniquedbandas@gmail.com>
2025-04-22 17:51:44 -0300

    Projeto finalizado!!!

--- src/database/database.js
+++ src/database/database.js
@@ -30,8 +30,37 @@ export class Database {
     this.#persist();
   }
 
-  select(table) {
+  select(table, filters) {
     let data = this.#database[table] ?? [];
+    if (filters) {
+      data = data.filter((row) => {
+        return Object.entries(filters).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase());
+        });
+        //some testa se pelo menos um dos valores bate
+        //se retorna vredadeiro, retornaa propria linha
+      });
+    }
     return data;
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = {
+        ...this.#database[table][rowIndex],
+        ...data,
+      };
+      this.#persist();
+    }
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1);
+      this.#persist();
+    }
   }
 }
--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,5 +1,8 @@
 import { create } from "../controllers/tickets/create.js";
 import { index } from "../controllers/tickets/index.js";
+import { update } from "../controllers/tickets/update.js";
+import { updateStatus } from "../controllers/tickets/updateStatus.js";
+import { remove } from "../controllers/tickets/remove.js";
 export const tickets = [
   {
     method: "POST",
@@ -10,6 +13,21 @@ export const tickets = [
     method: "GET",
     path: "/tickets",
     controller: index,
+  },
+  {
+    method: "PUT",
+    path: "/tickets/:id",
+    controller: update,
+  },
+  {
+    method: "PATCH",
+    path: "/tickets/:id/close",
+    controller: updateStatus,
+  },
+  {
+    method: "DELETE",
+    path: "/tickets/:id",
+    controller: remove,
   },
 ];
 
